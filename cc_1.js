867319c6e2e0a891792984a0d37d41e1422b0226
Nicole Dragassakis <niki_dragasakis@icloud.com>
2024-09-01 17:36:13 -0400

    Task 2 - Product Inventory Management

--- cc_1.js
+++ cc_1.js
@@ -1,7 +1,14 @@
-//Task 1: employee details
+//Task 1: Employee details
 let employeeName = "Jeniffer Lawrence";
 const employeeID ="B01990199"
 var isActive = true
-console.log(employeeName, typeof(customerName))
-console.log(employeeID, typeof(employeeID))
-console.log(isActive, typeof(isActive))
+console.log(employeeName, typeof(customerName));
+console.log(employeeID, typeof(employeeID));
+console.log(isActive, typeof(isActive));
+//Task 2: Product Inventory Management
+let products = ["Tires", "Rotors", "Brake Pads"];
+const productDetails = {
+    inStock: yes,
+    name: maintenance deal,
+    price: 500.00,
+} 
