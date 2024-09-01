b4094098d0da9251f7e9b18606c3d398075292c5
Nicole Dragassakis <niki_dragasakis@icloud.com>
2024-09-01 14:07:42 -0400

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,7 @@
+//Task 1: employee details
+let employeeName = "Jeniffer Lawrence";
+const employeeID ="B01990199"
+var isActive = true
+console.log(employeeName, typeof(customerName))
+console.log(employeeID, typeof(employeeID))
+console.log(isActive, typeof(isActive))
